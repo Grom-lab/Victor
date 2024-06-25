e67b433e0fc51621570e9f3ace4e60982b15b286
Grom-lab <54707947+Grom-lab@users.noreply.github.com>
2024-06-25 20:36:38 +1000

    bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,35 @@
+from telegram.ext import Updater, MessageHandler, Filters
 
+# Ваш токен, полученный от BotFather
+TOKEN = '7088612669:AAE5euMjd1V9fj4jkN8yj3TrroHykWxWEn8'
+
+def respond_to_viktorian(update, context):
+    # Получаем текст сообщения
+    message_text = update.message.text.lower()
+    
+    # Проверяем, содержит ли сообщение слово "Викториан"
+    if 'викториан' in message_text:
+        # Отправляем ответное сообщение
+        update.message.reply_text('К вашим услугам!')
+
+def main():
+    # Создаем Updater и передаем ему токен вашего бота
+    updater = Updater(TOKEN, use_context=True)
+    
+    # Получаем диспетчера для регистрации обработчиков
+    dp = updater.dispatcher
+    
+    # Создаем обработчик сообщений, который будет реагировать на все текстовые сообщения
+    text_handler = MessageHandler(Filters.text & (~Filters.command), respond_to_viktorian)
+    
+    # Регистрируем обработчик в диспетчере
+    dp.add_handler(text_handler)
+    
+    # Запускаем бота
+    updater.start_polling()
+    
+    # Останавливаем бота при завершении работы
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
